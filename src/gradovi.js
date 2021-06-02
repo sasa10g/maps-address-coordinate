682fc8b4a5335bf5c163887f3ff65e0c5f17ed59
Sasa <sasagvozdenovic@hotmail.com>
2021-06-02 02:34:06 +0200

    Added batch of cities

---
+++ src/gradovi.js
@@ -0,0 +1,1456 @@
+const gradovi = [
+  {
+    naziv: "ADA",
+  },
+  {
+    naziv: "ADORJAN",
+  },
+  {
+    naziv: "ALEKSANDROVAC (KRUSEVAC)",
+  },
+  {
+    naziv: "ALEKSINAC",
+  },
+  {
+    naziv: "ALIBUNAR",
+  },
+  {
+    naziv: "APATIN",
+  },
+  {
+    naziv: "ARADAC",
+  },
+  {
+    naziv: "ARANĐELOVAC",
+  },
+  {
+    naziv: "ARILJE",
+  },
+  {
+    naziv: "AZANJA",
+  },
+  {
+    naziv: "BABUŠNICA",
+  },
+  {
+    naziv: "BACINCI",
+  },
+  {
+    naziv: "BADOVINCI",
+  },
+  {
+    naziv: "BAJINA BAŠTA",
+  },
+  {
+    naziv: "BAJMOK",
+  },
+  {
+    naziv: "BALJEVAC (NA IBRU)",
+  },
+  {
+    naziv: "BANATSKA DUBICA",
+  },
+  {
+    naziv: "BANATSKA PALANKA",
+  },
+  {
+    naziv: "BANATSKA TOPOLA",
+  },
+  {
+    naziv: "BANATSKI BRESTOVAC",
+  },
+  {
+    naziv: "BANATSKI DESPOTOVAC",
+  },
+  {
+    naziv: "BANATSKI DVOR",
+  },
+  {
+    naziv: "BANATSKI KARLOVAC",
+  },
+  {
+    naziv: "BANATSKO KARADJORDJEVO",
+  },
+  {
+    naziv: "BANATSKO NOVO SELO",
+  },
+  {
+    naziv: "BANATSKO VELIKO SELO",
+  },
+  {
+    naziv: "BANJA KOVILJACA",
+  },
+  {
+    naziv: "BANJA LUKA",
+  },
+  {
+    naziv: "BANOVO POLJE",
+  },
+  {
+    naziv: "BARAJEVO",
+  },
+  {
+    naziv: "BARICE",
+  },
+  {
+    naziv: "BARIČ",
+  },
+  {
+    naziv: "BATAJNICA",
+  },
+  {
+    naziv: "BATOČINA",
+  },
+  {
+    naziv: "BATROVCI",
+  },
+  {
+    naziv: "BAVANISTE",
+  },
+  {
+    naziv: "BAČ",
+  },
+  {
+    naziv: "BAČINA",
+  },
+  {
+    naziv: "BAČKA PALANKA",
+  },
+  {
+    naziv: "BAČKA TOPOLA",
+  },
+  {
+    naziv: "BAČKI BRESTOVAC",
+  },
+  {
+    naziv: "BAČKI GRAČAC",
+  },
+  {
+    naziv: "BAČKI MONOŠTOR",
+  },
+  {
+    naziv: "BAČKI PETROVAC",
+  },
+  {
+    naziv: "BAČKO GRADIŠTE",
+  },
+  {
+    naziv: "BAČKO PETROVO SELO",
+  },
+  {
+    naziv: "BAŠAID",
+  },
+  {
+    naziv: "BEGALJICA",
+  },
+  {
+    naziv: "BEGEČ",
+  },
+  {
+    naziv: "BELA CRKVA",
+  },
+  {
+    naziv: "BELA CRKVA (KRUPANJ)",
+  },
+  {
+    naziv: "BELA PALANKA",
+  },
+  {
+    naziv: "BELE VODE",
+  },
+  {
+    naziv: "BELEGIŠ",
+  },
+  {
+    naziv: "BELO BLATO",
+  },
+  {
+    naziv: "BELOLJIN",
+  },
+  {
+    naziv: "BELUŠIĆ",
+  },
+  {
+    naziv: "BEOGRAD",
+  },
+  {
+    naziv: "BEOGRAD - STARI GRAD",
+  },
+  {
+    naziv: "BEOGRAD-BELI POTOK",
+  },
+  {
+    naziv: "BEOGRAD-CUKARICA",
+  },
+  {
+    naziv: "BEOGRAD-NOVI BEOGRAD",
+  },
+  {
+    naziv: "BEOGRAD-RAKOVICA",
+  },
+  {
+    naziv: "BEOGRAD-VOŽDOVAC",
+  },
+  {
+    naziv: "BEOGRAD-ZARKOVO",
+  },
+  {
+    naziv: "BEOGRAD-ZEMUN",
+  },
+  {
+    naziv: "BEOGRAD-ZVEZDARA",
+  },
+  {
+    naziv: "BEOČIN",
+  },
+  {
+    naziv: "BESKA",
+  },
+  {
+    naziv: "BEZDAN",
+  },
+  {
+    naziv: "BEČEJ",
+  },
+  {
+    naziv: "BEČMEN",
+  },
+  {
+    naziv: "BIKOVO",
+  },
+  {
+    naziv: "BILJAČA",
+  },
+  {
+    naziv: "BLACE",
+  },
+  {
+    naziv: "BOGATIĆ",
+  },
+  {
+    naziv: "BOGOJEVCE",
+  },
+  {
+    naziv: "BOGOJEVO",
+  },
+  {
+    naziv: "BOJNIK",
+  },
+  {
+    naziv: "BOLEČ",
+  },
+  {
+    naziv: "BOLJEVAC",
+  },
+  {
+    naziv: "BOR",
+  },
+  {
+    naziv: "BORČA",
+  },
+  {
+    naziv: "BOTOŠ",
+  },
+  {
+    naziv: "BRESTOVAC",
+  },
+  {
+    naziv: "BRUS",
+  },
+  {
+    naziv: "BUCURESTI",
+  },
+  {
+    naziv: "BUFALO GROVE",
+  },
+  {
+    naziv: "BUJANOVAC",
+  },
+  {
+    naziv: "BURNABY",
+  },
+  {
+    naziv: "BUSILOVAC",
+  },
+  {
+    naziv: "BUĐANOVCI",
+  },
+  {
+    naziv: "Backo Dobro Polje",
+  },
+  {
+    naziv: "CACAK",
+  },
+  {
+    naziv: "CEREVIC",
+  },
+  {
+    naziv: "CEROVAC (SABACKI)",
+  },
+  {
+    naziv: "CREPAJA",
+  },
+  {
+    naziv: "CRLJENAC",
+  },
+  {
+    naziv: "CRVENA REKA",
+  },
+  {
+    naziv: "CRVENKA",
+  },
+  {
+    naziv: "DEBELJAČA",
+  },
+  {
+    naziv: "DESPOTOVAC",
+  },
+  {
+    naziv: "DESPOTOVO",
+  },
+  {
+    naziv: "DEČ",
+  },
+  {
+    naziv: "DIMITROVGRAD",
+  },
+  {
+    naziv: "DIVCI",
+  },
+  {
+    naziv: "DIVOS",
+  },
+  {
+    naziv: "DOBANOVCI",
+  },
+  {
+    naziv: "DOLJEVAC",
+  },
+  {
+    naziv: "DOLOVO",
+  },
+  {
+    naziv: "DONJA LIVADICA",
+  },
+  {
+    naziv: "DONJE CRNJELJEVO",
+  },
+  {
+    naziv: "DOROSLOVO",
+  },
+  {
+    naziv: "DRAGINAC",
+  },
+  {
+    naziv: "DRAŽEVAC",
+  },
+  {
+    naziv: "DUBAI",
+  },
+  {
+    naziv: "DUBOKA",
+  },
+  {
+    naziv: "DUBOVAC",
+  },
+  {
+    naziv: "DUBRAVICA(POZAREVAC)",
+  },
+  {
+    naziv: "DUDOVICA",
+  },
+  {
+    naziv: "ELEMIR",
+  },
+  {
+    naziv: "ERDEVIK",
+  },
+  {
+    naziv: "EČKA",
+  },
+  {
+    naziv: "FEKETIĆ",
+  },
+  {
+    naziv: "FUTOG",
+  },
+  {
+    naziv: "GADŽIN HAN",
+  },
+  {
+    naziv: "GAJ",
+  },
+  {
+    naziv: "GAKOVO",
+  },
+  {
+    naziv: "GLOGONJ",
+  },
+  {
+    naziv: "GLUŠCI",
+  },
+  {
+    naziv: "GOLUBAC",
+  },
+  {
+    naziv: "GORNJI MILANOVAC",
+  },
+  {
+    naziv: "GRDELICA",
+  },
+  {
+    naziv: "GREBENAC",
+  },
+  {
+    naziv: "GRGUREVCI",
+  },
+  {
+    naziv: "GROCKA",
+  },
+  {
+    naziv: "GUNAROŠ",
+  },
+  {
+    naziv: "Gornji Breg",
+  },
+  {
+    naziv: "HAJDUKOVO",
+  },
+  {
+    naziv: "HAJDUČICA",
+  },
+  {
+    naziv: "HORGOŠ",
+  },
+  {
+    naziv: "HRTKOVCI",
+  },
+  {
+    naziv: "IDVOR",
+  },
+  {
+    naziv: "ILANDŽA",
+  },
+  {
+    naziv: "ILINCI",
+  },
+  {
+    naziv: "ILLINOIS",
+  },
+  {
+    naziv: "INNSBRUCK  A-6020",
+  },
+  {
+    naziv: "INĐIJA",
+  },
+  {
+    naziv: "IRIG",
+  },
+  {
+    naziv: "IVANJICA",
+  },
+  {
+    naziv: "IĐOŠ",
+  },
+  {
+    naziv: "JAGODINA",
+  },
+  {
+    naziv: "JAJINCI",
+  },
+  {
+    naziv: "JAKOVO",
+  },
+  {
+    naziv: "JAMENA",
+  },
+  {
+    naziv: "JANKOV MOST KOD ZRENJANINA",
+  },
+  {
+    naziv: "JARAK",
+  },
+  {
+    naziv: "JASENOVO",
+  },
+  {
+    naziv: "JAZAK",
+  },
+  {
+    naziv: "JAŠA TOMIĆ",
+  },
+  {
+    naziv: "JOSANICKA BANJA",
+  },
+  {
+    naziv: "JUNKOVAC",
+  },
+  {
+    naziv: "Jarkovac",
+  },
+  {
+    naziv: "KALUĐERICA",
+  },
+  {
+    naziv: "KANJIŽA",
+  },
+  {
+    naziv: "KARAVUKOVO",
+  },
+  {
+    naziv: "KATUN",
+  },
+  {
+    naziv: "KAĆ",
+  },
+  {
+    naziv: "KAČAREVO",
+  },
+  {
+    naziv: "KELEBIJA",
+  },
+  {
+    naziv: "KEVI",
+  },
+  {
+    naziv: "KIJEV",
+  },
+  {
+    naziv: "KIKINDA",
+  },
+  {
+    naziv: "KISAČ",
+  },
+  {
+    naziv: "KLADOVO",
+  },
+  {
+    naziv: "KLENAK",
+  },
+  {
+    naziv: "KLENJE",
+  },
+  {
+    naziv: "KLIČEVAC",
+  },
+  {
+    naziv: "KLJAJIĆEVO",
+  },
+  {
+    naziv: "KNIĆ",
+  },
+  {
+    naziv: "KOCELJEVA",
+  },
+  {
+    naziv: "KOLUT",
+  },
+  {
+    naziv: "KONAK",
+  },
+  {
+    naziv: "KONAREVO",
+  },
+  {
+    naziv: "KOPAONIK",
+  },
+  {
+    naziv: "KOSIJERIĆ",
+  },
+  {
+    naziv: "KOSTOLAC",
+  },
+  {
+    naziv: "KOVAČICA",
+  },
+  {
+    naziv: "KOVILJ",
+  },
+  {
+    naziv: "KOVIN",
+  },
+  {
+    naziv: "KRAGUJEVAC",
+  },
+  {
+    naziv: "KRAJIŠNIK",
+  },
+  {
+    naziv: "KRALJEVO",
+  },
+  {
+    naziv: "KRCEDIN",
+  },
+  {
+    naziv: "KREPOLJIN",
+  },
+  {
+    naziv: "KRIVAJA",
+  },
+  {
+    naziv: "KRNJACA-KOTEZ",
+  },
+  {
+    naziv: "KRNJEVO",
+  },
+  {
+    naziv: "KRUPANJ",
+  },
+  {
+    naziv: "KRUŠEVAC",
+  },
+  {
+    naziv: "KUCURA",
+  },
+  {
+    naziv: "KULA",
+  },
+  {
+    naziv: "KUMANE",
+  },
+  {
+    naziv: "KUPINOVO",
+  },
+  {
+    naziv: "KURŠUMLIJA",
+  },
+  {
+    naziv: "KUZMIN",
+  },
+  {
+    naziv: "KUČEVO",
+  },
+  {
+    naziv: "Knićanin",
+  },
+  {
+    naziv: "LAJKOVAC",
+  },
+  {
+    naziv: "LALIĆ",
+  },
+  {
+    naziv: "LAPOVO",
+  },
+  {
+    naziv: "LAZAREVAC.",
+  },
+  {
+    naziv: "LAZAREVO",
+  },
+  {
+    naziv: "LAZNICA",
+  },
+  {
+    naziv: "LAĆARAK",
+  },
+  {
+    naziv: "LAĐEVCI",
+  },
+  {
+    naziv: "LEBANE",
+  },
+  {
+    naziv: "LEPOSAVIĆ",
+  },
+  {
+    naziv: "LESKOVAC",
+  },
+  {
+    naziv: "LEŠAK",
+  },
+  {
+    naziv: "LEŠNICA",
+  },
+  {
+    naziv: "LEŠTANE",
+  },
+  {
+    naziv: "LINCOLNWOOD",
+  },
+  {
+    naziv: "LIPOLIST",
+  },
+  {
+    naziv: "LJIG",
+  },
+  {
+    naziv: "LJUBLJANA",
+  },
+  {
+    naziv: "LJUBOVIJA",
+  },
+  {
+    naziv: "LJUKOVO",
+  },
+  {
+    naziv: "LOVĆENAC",
+  },
+  {
+    naziv: "LOZNICA",
+  },
+  {
+    naziv: "LOZOVIK",
+  },
+  {
+    naziv: "LUGAVČINA",
+  },
+  {
+    naziv: "LUKIĆEVO",
+  },
+  {
+    naziv: "LUČANI",
+  },
+  {
+    naziv: "MAGLIĆ",
+  },
+  {
+    naziv: "MAJILOVAC",
+  },
+  {
+    naziv: "MALA MOSTANICA",
+  },
+  {
+    naziv: "MALE PIJACE",
+  },
+  {
+    naziv: "MALI IĐOŠ",
+  },
+  {
+    naziv: "MALI POZAREVAC",
+  },
+  {
+    naziv: "MALI ZVORNIK",
+  },
+  {
+    naziv: "MALO CRNIĆE",
+  },
+  {
+    naziv: "MARADIK",
+  },
+  {
+    naziv: "MARKOVAC",
+  },
+  {
+    naziv: "MAČKET",
+  },
+  {
+    naziv: "MAČVANSKA MITROVICA",
+  },
+  {
+    naziv: "MEDOŠEVAC",
+  },
+  {
+    naziv: "MEDVEĐA",
+  },
+  {
+    naziv: "MELENCI",
+  },
+  {
+    naziv: "MELJAK",
+  },
+  {
+    naziv: "MEROŠINA",
+  },
+  {
+    naziv: "MIHAJLOVO",
+  },
+  {
+    naziv: "MIJATOVAC",
+  },
+  {
+    naziv: "MIONICA",
+  },
+  {
+    naziv: "MLADENOVAC",
+  },
+  {
+    naziv: "MLADENOVO",
+  },
+  {
+    naziv: "MOKRIN",
+  },
+  {
+    naziv: "MOL",
+  },
+  {
+    naziv: "MOROVIC",
+  },
+  {
+    naziv: "MOSKVA",
+  },
+  {
+    naziv: "MRAMOR (kod Niša)",
+  },
+  {
+    naziv: "Mrčajevci",
+  },
+  {
+    naziv: "NADALJ",
+  },
+  {
+    naziv: "NAKOVO",
+  },
+  {
+    naziv: "NEGOTIN (Krajina)",
+  },
+  {
+    naziv: "NERESNICA",
+  },
+  {
+    naziv: "NEUZINA",
+  },
+  {
+    naziv: "NIŠ",
+  },
+  {
+    naziv: "NIŠKA BANJA",
+  },
+  {
+    naziv: "NOVA CRNJA",
+  },
+  {
+    naziv: "NOVA PAZOVA",
+  },
+  {
+    naziv: "NOVA VAROŠ",
+  },
+  {
+    naziv: "NOVI BANOVCI",
+  },
+  {
+    naziv: "NOVI BEČEJ",
+  },
+  {
+    naziv: "NOVI KARLOVCI",
+  },
+  {
+    naziv: "NOVI KNEŽEVAC",
+  },
+  {
+    naziv: "NOVI PAZAR",
+  },
+  {
+    naziv: "NOVI SAD",
+  },
+  {
+    naziv: "NOVI SLANKAMEN",
+  },
+  {
+    naziv: "NOVO MILOŠEVO",
+  },
+  {
+    naziv: "Novi Kozarci",
+  },
+  {
+    naziv: "Novi ednik",
+  },
+  {
+    naziv: "Novo Orahovo",
+  },
+  {
+    naziv: "OBRENOVAC",
+  },
+  {
+    naziv: "OBROVAC",
+  },
+  {
+    naziv: "ODŽACI",
+  },
+  {
+    naziv: "OPOVO",
+  },
+  {
+    naziv: "ORLOVAT",
+  },
+  {
+    naziv: "OROM",
+  },
+  {
+    naziv: "OSANICA",
+  },
+  {
+    naziv: "OSEČINA",
+  },
+  {
+    naziv: "OSIPAONICA",
+  },
+  {
+    naziv: "OSTRUZNICA",
+  },
+  {
+    naziv: "PADINA",
+  },
+  {
+    naziv: "PALIĆ",
+  },
+  {
+    naziv: "PANCEVO",
+  },
+  {
+    naziv: "PANČEVO",
+  },
+  {
+    naziv: "PARAĆIN",
+  },
+  {
+    naziv: "PAVLIŠ",
+  },
+  {
+    naziv: "PAČIR",
+  },
+  {
+    naziv: "PERLEZ",
+  },
+  {
+    naziv: "PETROVAC (na Mlavi)",
+  },
+  {
+    naziv: "PETROVARADIN",
+  },
+  {
+    naziv: "PETROVČIĆ",
+  },
+  {
+    naziv: "PEĆINCI",
+  },
+  {
+    naziv: "PEČENJEVCE",
+  },
+  {
+    naziv: "PIROT",
+  },
+  {
+    naziv: "PLANDIŠTE",
+  },
+  {
+    naziv: "PLAVNA",
+  },
+  {
+    naziv: "PODGORICA",
+  },
+  {
+    naziv: "PODUNAVCI",
+  },
+  {
+    naziv: "POJATE",
+  },
+  {
+    naziv: "PORODIN (Žabari)",
+  },
+  {
+    naziv: "POŽAREVAC",
+  },
+  {
+    naziv: "POŽEGA",
+  },
+  {
+    naziv: "PREDEJANE",
+  },
+  {
+    naziv: "PRELJINA",
+  },
+  {
+    naziv: "PRIBOJ",
+  },
+  {
+    naziv: "PRIGREVICA",
+  },
+  {
+    naziv: "PRIJEPOLJE",
+  },
+  {
+    naziv: "PRNJAVOR (Mačvanski)",
+  },
+  {
+    naziv: "PROGAR",
+  },
+  {
+    naziv: "PROKUPLJE",
+  },
+  {
+    naziv: "PUKOVAC",
+  },
+  {
+    naziv: "RADALJ",
+  },
+  {
+    naziv: "RADINAC",
+  },
+  {
+    naziv: "RADIČEVIĆ",
+  },
+  {
+    naziv: "RADLJEVO",
+  },
+  {
+    naziv: "RALJA",
+  },
+  {
+    naziv: "RATINA",
+  },
+  {
+    naziv: "RAVNO SELO",
+  },
+  {
+    naziv: "RAČA (kragujevačka)",
+  },
+  {
+    naziv: "RAŠKA",
+  },
+  {
+    naziv: "REKOVAC",
+  },
+  {
+    naziv: "RESAVICA",
+  },
+  {
+    naziv: "RESNIK",
+  },
+  {
+    naziv: "REUTLINGEN",
+  },
+  {
+    naziv: "RIBARSKA BANJA",
+  },
+  {
+    naziv: "RIPANJ",
+  },
+  {
+    naziv: "RIĐICA",
+  },
+  {
+    naziv: "ROTH",
+  },
+  {
+    naziv: "RUMA",
+  },
+  {
+    naziv: "RUMENKA",
+  },
+  {
+    naziv: "RUSKI KRSTUR",
+  },
+  {
+    naziv: "RUŠANJ",
+  },
+  {
+    naziv: "Rastina",
+  },
+  {
+    naziv: "Razbojna",
+  },
+  {
+    naziv: "Ražanj",
+  },
+  {
+    naziv: "Ribare (kod Jagodine)",
+  },
+  {
+    naziv: "SAJAN",
+  },
+  {
+    naziv: "SAJKAS",
+  },
+  {
+    naziv: "SAMOŠ",
+  },
+  {
+    naziv: "SANAD",
+  },
+  {
+    naziv: "SANKT-PETERBURG",
+  },
+  {
+    naziv: "SARAORCI",
+  },
+  {
+    naziv: "SAVINO SELO",
+  },
+  {
+    naziv: "SELENČA",
+  },
+  {
+    naziv: "SELEVAC",
+  },
+  {
+    naziv: "SENTA",
+  },
+  {
+    naziv: "SETONJE",
+  },
+  {
+    naziv: "SEVOJNO",
+  },
+  {
+    naziv: "SIMANOVCI",
+  },
+  {
+    naziv: "SIVAC",
+  },
+  {
+    naziv: "SKOBALJ",
+  },
+  {
+    naziv: "SKOPJE",
+  },
+  {
+    naziv: "SMEDEREVO",
+  },
+  {
+    naziv: "SMEDEREVSKA PALANKA",
+  },
+  {
+    naziv: "SMOLJINAC",
+  },
+  {
+    naziv: "SOKO BANJA",
+  },
+  {
+    naziv: "SOMBOR",
+  },
+  {
+    naziv: "SOPOT",
+  },
+  {
+    naziv: "SRBOBRAN",
+  },
+  {
+    naziv: "SREDNJEVO",
+  },
+  {
+    naziv: "SREMSKA KAMENICA",
+  },
+  {
+    naziv: "SREMSKA MITROVICA",
+  },
+  {
+    naziv: "SREMSKI KARLOVCI",
+  },
+  {
+    naziv: "SREMČICA",
+  },
+  {
+    naziv: "SRPSKA CRNJA",
+  },
+  {
+    naziv: "SRPSKI ITEBEJ",
+  },
+  {
+    naziv: "SRPSKI MILETIĆ",
+  },
+  {
+    naziv: "STAJIĆEVO",
+  },
+  {
+    naziv: "STANIŠIĆ",
+  },
+  {
+    naziv: "STAPAR",
+  },
+  {
+    naziv: "STARA PAZOVA",
+  },
+  {
+    naziv: "STARI SLANKAMEN",
+  },
+  {
+    naziv: "STARČEVO",
+  },
+  {
+    naziv: "SUBOTICA",
+  },
+  {
+    naziv: "SUBOTISTE",
+  },
+  {
+    naziv: "SURDUK",
+  },
+  {
+    naziv: "SURDULICA",
+  },
+  {
+    naziv: "SURČIN",
+  },
+  {
+    naziv: "SUSEK",
+  },
+  {
+    naziv: "SVILAJNAC",
+  },
+  {
+    naziv: "SVILOJEVO",
+  },
+  {
+    naziv: "SVRLJIG",
+  },
+  {
+    naziv: "SYDNEY",
+  },
+  {
+    naziv: "Seleu",
+  },
+  {
+    naziv: "Sikirica",
+  },
+  {
+    naziv: "Sonta",
+  },
+  {
+    naziv: "Srpski Krstur",
+  },
+  {
+    naziv: "Stari Lec",
+  },
+  {
+    naziv: "Stopanja",
+  },
+  {
+    naziv: "Subang Jaya",
+  },
+  {
+    naziv: "Supska",
+  },
+  {
+    naziv: "TARAŠ",
+  },
+  {
+    naziv: "TEMERIN",
+  },
+  {
+    naziv: "THERESIEFELD",
+  },
+  {
+    naziv: "TOBA",
+  },
+  {
+    naziv: "TOPOLA",
+  },
+  {
+    naziv: "TORNJOŠ",
+  },
+  {
+    naziv: "TOVARIŠEVO",
+  },
+  {
+    naziv: "TRSTENIK",
+  },
+  {
+    naziv: "UB",
+  },
+  {
+    naziv: "UGRINOVCI",
+  },
+  {
+    naziv: "UMKA",
+  },
+  {
+    naziv: "UTRINE",
+  },
+  {
+    naziv: "Umčari",
+  },
+  {
+    naziv: "Uzdin",
+  },
+  {
+    naziv: "UŽICE",
+  },
+  {
+    naziv: "VAJSKA",
+  },
+  {
+    naziv: "VALJEVO",
+  },
+  {
+    naziv: "VARVARIN",
+  },
+  {
+    naziv: "VELIKA GREDA",
+  },
+  {
+    naziv: "VELIKA MOSTANICA",
+  },
+  {
+    naziv: "VELIKA PLANA",
+  },
+  {
+    naziv: "VELIKI CRLJENI",
+  },
+  {
+    naziv: "VELIKI RADINCI",
+  },
+  {
+    naziv: "VELIKO GRADIŠTE",
+  },
+  {
+    naziv: "VELIKO LAOLE",
+  },
+  {
+    naziv: "VETERNIK",
+  },
+  {
+    naziv: "VICTORIA",
+  },
+  {
+    naziv: "VINČA",
+  },
+  {
+    naziv: "VIŠNJEVAC",
+  },
+  {
+    naziv: "VLADIMIRCI",
+  },
+  {
+    naziv: "VLADIMIROVAC",
+  },
+  {
+    naziv: "VLADIVOSTOK",
+  },
+  {
+    naziv: "VLADIČIN HAN",
+  },
+  {
+    naziv: "VLASINA RID",
+  },
+  {
+    naziv: "VLASOTINCE",
+  },
+  {
+    naziv: "VOLUJA",
+  },
+  {
+    naziv: "VORONEZ",
+  },
+  {
+    naziv: "VRANJE",
+  },
+  {
+    naziv: "VRANJSKA BANJA",
+  },
+  {
+    naziv: "VRANOVO",
+  },
+  {
+    naziv: "VRBAS",
+  },
+  {
+    naziv: "VRCIN",
+  },
+  {
+    naziv: "VRDNIK",
+  },
+  {
+    naziv: "VRGORAC",
+  },
+  {
+    naziv: "VRNJAČKA BANJA",
+  },
+  {
+    naziv: "VRŠAC",
+  },
+  {
+    naziv: "VUČJE",
+  },
+  {
+    naziv: "Vladičin Han",
+  },
+  {
+    naziv: "Vlasina Okruglica",
+  },
+  {
+    naziv: "Vranić",
+  },
+  {
+    naziv: "Vrnjci",
+  },
+  {
+    naziv: "ZAGREB",
+  },
+  {
+    naziv: "ZAJEČAR",
+  },
+  {
+    naziv: "ZASAVICA",
+  },
+  {
+    naziv: "ZAVLAKA",
+  },
+  {
+    naziv: "ZEMUN POLJE",
+  },
+  {
+    naziv: "ZITISTE",
+  },
+  {
+    naziv: "ZLATIBOR",
+  },
+  {
+    naziv: "ZMAJEVO",
+  },
+  {
+    naziv: "ZRENJANIN",
+  },
+  {
+    naziv: "ĆIĆEVAC",
+  },
+  {
+    naziv: "ĆUPRIJA",
+  },
+  {
+    naziv: "ČAJETINA",
+  },
+  {
+    naziv: "ČANTAVIR",
+  },
+  {
+    naziv: "ČAČAK",
+  },
+  {
+    naziv: "ČELAREVO",
+  },
+  {
+    naziv: "ČENTA",
+  },
+  {
+    naziv: "ČESTEREG",
+  },
+  {
+    naziv: "ČONOPLJA",
+  },
+  {
+    naziv: "ČURUG",
+  },
+  {
+    naziv: "ĐURĐEVO",
+  },
+  {
+    naziv: "ŠABAC",
+  },
+  {
+    naziv: "ŠAŠINCI",
+  },
+  {
+    naziv: "ŠID",
+  },
+  {
+    naziv: "Šurjan",
+  },
+  {
+    naziv: "ŽABALJ",
+  },
+  {
+    naziv: "ŽABARI",
+  },
+  {
+    naziv: "ŽAGUBICA",
+  },
+  {
+    naziv: "ŽELEZNIK",
+  },
+  {
+    naziv: "ŽITKOVAC",
+  },
+];
+
+export default gradovi;
